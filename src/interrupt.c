6198befc4f4fc6943560db227a18877fb33c6f39
jmlizardi <justmonkey16@gmail.com>
2025-09-21 18:44:45 -0500

    Fighting these interrupts and handlers still

--- src/interrupt.c
+++ src/interrupt.c
@@ -314,17 +314,17 @@ __attribute__((interrupt)) void stack_exception_handler(struct interrupt_frame* 
 
 
 //__attribute__((interrupt)) void general_protection_handler(struct interrupt_frame* frame)
-void general_protection_handler(struct interrupt_frame* frame)
+__attribute__((interrupt)) void general_protection_handler(struct interrupt_frame* frame)
 {
     asm("cli");
     /* do something */
-    while(1);
+    outb(0x20,0x20);
 }
 //void page_fault_handler(struct interrupt_frame* frame)
-void page_fault_handler(struct process_context_with_error* ctx)
+__attribute__((interrupt)) void page_fault_handler(struct interrupt_frame* frame)
 {
     asm("cli");
-    while(1);
+    outb(0x20,0x20);
 }
 
 
